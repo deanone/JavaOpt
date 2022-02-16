d26c66ec64fbad775a1a272b913becf7af6dc5dd
deanone <athanasios.salamanis@gmail.com>
2022-02-16 17:14:05 +0200

    Refactoring. Checking the best indentation style (4 spaces).

--- DistanceCalculator.java
+++ DistanceCalculator.java
@@ -1,36 +1,36 @@
 public class DistanceCalculator
 {
-	/**
-	 * The first vector.
-	 */
-	double[] x;
+    /**
+     * The first vector.
+     */
+    double[] x;
 
-	/**
-	 * The second vector.
-	 */
-	double[] y;
+    /**
+     * The second vector.
+     */
+    double[] y;
 
-	/**
-	 * Constructor.
-	 */
-	public DistanceCalculator(double[] x, double[] y)
-	{
-		this.x = x;
-		this.y = y;
-	}
+    /**
+     * Constructor.
+     */
+    public DistanceCalculator(double[] x, double[] y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 
-	/**
-	 * Computes the Euclidean distance between two vectors.
-	 * @return the Euclidean distance.
-	 */
-	public double euclideanDistance()
-	{
-		double dist = 0.0;
-		for (int i = 0; i < x.length; ++i)
-		{
-			dist += Math.pow(x[i] - y[i], 2);
-		}
-		dist = Math.sqrt(dist);
-		return dist;
-	}
+    /**
+     * Computes the Euclidean distance between two vectors.
+     * @return the Euclidean distance.
+     */
+    public double euclideanDistance()
+    {
+        double dist = 0.0;
+        for (int i = 0; i < x.length; ++i)
+        {
+            dist += Math.pow(x[i] - y[i], 2);
+        }
+        dist = Math.sqrt(dist);
+        return dist;
+    }
 }
