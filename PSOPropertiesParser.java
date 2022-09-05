fc84a3693de5cbfd6207b6e7bde1e7d513404a24
Athanasios Salamanis <asal@iti.gr>
2022-09-05 10:28:57 +0300

    Major refactoring (4) - PSOPropertiesParser class.

--- PSOPropertiesParser.java
+++ PSOPropertiesParser.java
@@ -1,29 +1,32 @@
 import java.io.InputStream;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.HashMap;
 import java.nio.file.Paths;
 
-public class PSOPropertiesParser
-{
+/**
+ * @author A. Salamanis
+ * @version 0.1
+ * @since 2020-04-25
+ *
+ * PSOPropertiesParser class: The class representing a properties file (.properties) parse.
+ */
+public class PSOPropertiesParser {
     HashMap<String, String> propertiesMap;
     
     /**
      * Reads the values of the properties from the properties file.
      */
-    public void readPropertiesValues()
-    {
+    public void readPropertiesValues() {
         try
         {
             propertiesMap = new HashMap<String, String>();
             Properties prop = new Properties();
             String cwd = Paths.get(".").toAbsolutePath().normalize().toString();
-            String propFileName = cwd + "/PSO.properties";
-            InputStream inputStream =  new FileInputStream(propFileName);
+            String propFilename = cwd + "/PSO.properties";
+            InputStream inputStream =  new FileInputStream(propFilename);
 
-            if (inputStream != null) 
-            {
+            if (inputStream != null) {
                 prop.load(inputStream);
 
                 // set the properties' values
@@ -40,9 +43,7 @@ public class PSOPropertiesParser
             }
             // TODO: Implement handler of the else case.
             inputStream.close();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
     }
@@ -52,8 +53,7 @@ public class PSOPropertiesParser
      * @param propertyName the name of the property.
      * @return the value of the property (as integer).
      */
-    int getPropertyAsInteger(String propertyName)
-    {
+    int getPropertyAsInteger(String propertyName) {
         return Integer.parseInt(propertiesMap.get(propertyName));
     }
 
@@ -62,8 +62,7 @@ public class PSOPropertiesParser
      * @param propertyName the name of the property.
      * @return the value of the property (as double).
      */
-    double getPropertyAsDouble(String propertyName)
-    {
+    double getPropertyAsDouble(String propertyName) {
         return Double.parseDouble(propertiesMap.get(propertyName));
     }
 }
